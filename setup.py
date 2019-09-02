861aa8edc4036041cbdacc089b47f6811498195b
Minh-Tri <pmtri80@gmail.com>
2019-09-02 17:30:25 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dataframemt',
-    version='0.2.1',
+    version='0.2.2',
     description="Extra Python modules to deal with dataframes for Minh-Tri Pham",
     author=["Minh-Tri Pham"],
     packages=find_packages(),
