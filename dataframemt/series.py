15e3c371d1f0e14bfea1cdee0416dc93d48ca1d6
Minh-Tri <minhtri.pham@winnowsolutions.com>
2019-10-17 15:39:06 +0100

    v0.3.8: moved functions related to pandas.Series to the seriesmt repo

--- dataframemt/series.py
+++ dataframemt/series.py
@@ -1,44 +1,5 @@
-'''Extra modules to augment pandas.'''
+'''Only for backward compatibility. You should import seriesmt or seriesmt.series instead.'''
 
-
-import pandas as _pd
-
-
-def Series4json(obj):
-    '''Converts a json-like object in to a pandas.Series.
-
-    Parameters
-    ==========
-    obj : json_like
-        a json-like object
-
-    Returns
-    =======
-    pandas.Series
-        output Series
-
-    Notes
-    =====
-    A json-like object contains 2 array-likes, each has length K. The first array represents the index array. The second array represents the value array.
-    '''
-    return _pd.Series(index=obj[0], data=obj[1])
-
-
-def json4Series(obj):
-    '''Converts a pandas.Series into a json-like object.
-
-    Parameters
-    ==========
-    obj : pandas.Series
-        input Series
-
-    Returns
-    =======
-    json_like
-        a json-like object
-
-    Notes
-    =====
-    A json-like object contains 2 array-likes, each has length K. The first array represents the index array. The second array represents the value array.
-    '''
-    return [obj.index.tolist(), obj.tolist()]
+from seriesmt.series import *
+from basemt import logger
+logger.warn("dataframemt.series is an old module. Its existence is only for backward compatibility. Please import seriesmt instead.")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dataframemt',
-    version='0.3.7',
+    version='0.3.8',
     description="Extra Python modules to deal with dataframes for Minh-Tri Pham",
     author=["Minh-Tri Pham"],
     packages=find_packages(),
@@ -16,5 +16,7 @@ setup(
         # 'mysql', # for mysql access
         'dask[dataframe]',  # for reading chunks of CSV files in parallel
         'basemt',  # Minh-Tri's base modules for logging and multi-threading
+        # Minh-Tri modules for dealing with pandas.Series. Only for backward compatibility. Currently dataframemt does not require seriesmt.
+        'seriesmt',
     ],
 )
