2c18608e3ab8f960c290af729b6028903bf83fee
Minh-Tri <minhtri.pham@winnowsolutions.com>
2020-02-03 05:02:24 +0000

    v0.4.1: now with list_frame_dependencies()

--- sqlmt/psql.py
+++ sqlmt/psql.py
@@ -457,6 +457,32 @@ def get_frame_length(frame_name, conn, schema=None, nb_trials=3, logger=None):
     return read_sql_query("SELECT COUNT(*) a FROM {};".format(frame_sql_str), conn, nb_trials=nb_trials, logger=logger)['a'][0]
 
 
+def get_frame_dependencies(frame_name, conn, schema=None, nb_trials=3, logger=None):
+    '''Gets the list of all frames that depend on the given frame.
+    '''
+    query_str = """
+        SELECT dependent_ns.nspname as dependent_schema
+        , dependent_view.relname as dependent_view 
+        , source_ns.nspname as source_schema
+        , source_table.relname as source_table
+        , pg_attribute.attname as column_name
+        FROM pg_depend 
+        JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid 
+        JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid 
+        JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid 
+        JOIN pg_attribute ON pg_depend.refobjid = pg_attribute.attrelid 
+            AND pg_depend.refobjsubid = pg_attribute.attnum 
+        JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
+        JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
+        WHERE 
+        source_ns.nspname = '{}'
+        AND source_table.relname = '{}'
+        AND pg_attribute.attnum > 0 
+        ORDER BY 1,2;
+    """.format('public' if schema is None else schema, frame_name)
+    return read_sql_query(query_str, conn, nb_trials=nb_trials, logger=logger)
+
+
 def get_view_sql_code(view_name, conn, schema=None, nb_trials=3, logger=None):
     '''Gets the SQL string of a view.
 
