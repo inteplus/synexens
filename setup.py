6693b90bf5fa0cfebbf56111f2c0960dbe926bd3
Minh-Tri Pham <pmtri80@gmail.com>
2024-01-22 12:51:01 +0000

    well

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 import platform
 
+from mt import path, shutil
+
 from setuptools import setup, find_packages
 from synexens.version import version
 
@@ -11,6 +13,12 @@ elif platform.machine() == "aarch64":
     plat = "arm64"
 else:
     raise NotImplementedError
+setup_dirpath = path.dirname(__file__)
+src_dirpath = path.join(setup_dirpath, "sysdk", "lib", plat)
+dst_dirpath = path.join(setup_dirpath, "synexens")
+for filepath in path.glob(src_dirpath + "/*.so"):
+    dst_filepath = path.join(dst_dirpath, path.basename(filepath))
+    shutil.copyfile(filepath, dst_filepath)
 
 setup(
     name="synexens",
@@ -19,6 +27,7 @@ setup(
     author=["Minh-Tri Pham"],
     packages=find_packages(),
     include_package_data=True,
+    package_data={"": ["*.so"]},
     install_requires=[
         "mtbase",  # for numpy access
         "cython",  # for wrapping purposes
