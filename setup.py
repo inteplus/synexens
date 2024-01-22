fa56fafa2f3838008b6700ce0bc3b85f8723c87c
Minh-Tri Pham <pmtri80@gmail.com>
2024-01-22 12:38:52 +0000

    well

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     description="Wrapper for Synexens SDK to access their LiDAR devices",
     author=["Minh-Tri Pham"],
     packages=find_packages(),
-    package_data={"synexens.sysdk.lib.amd64": ["*.so"], "": ["*.so"]},
     include_package_data=True,
     install_requires=[
         "mtbase",  # for numpy access
