fba305950b65bad88827785ed8cfb4926ac9796f
Minh-Tri Pham <pmtri80@gmail.com>
2024-01-22 09:59:24 +0000

    v0.0: a new beginning

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
-from synexens.version import VERSION
+from synexens.version import version
 
 setup(
     name="synexens",
-    version=VERSION,
+    version=version,
     description="Wrapper for Synexens SDK to access their LiDAR devices",
     author=["Minh-Tri Pham"],
     packages=find_packages(),
     install_requires=[
         "mtbase",  # for numpy access
+        "cython",  # for wrapping purposes
     ],
 )
