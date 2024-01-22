34d991901f1a9a465c70cd705882a4bec1998330
Minh-Tri Pham <pmtri80@gmail.com>
2024-01-22 12:27:06 +0000

    well

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+import platform
+
 from setuptools import setup, find_packages
 from synexens.version import version
+
+if platform.machine() == "x86_64":
+    plat = "amd64"
+elif platform.machine() == "aarch64":
+    plat = "arm64"
+else:
+    raise NotImplementedError
 
 setup(
     name="synexens",
@@ -9,6 +18,8 @@ setup(
     description="Wrapper for Synexens SDK to access their LiDAR devices",
     author=["Minh-Tri Pham"],
     packages=find_packages(),
+    package_data={"": ["*.so"]},
+    include_package_data=True,
     install_requires=[
         "mtbase",  # for numpy access
         "cython",  # for wrapping purposes
