a14870f456480c09232cb4c2aeac01b7dc2efdc9
Minh-Tri Pham <pmtri80@gmail.com>
2024-01-22 14:54:06 +0000

    bug fix

--- setup.py
+++ setup.py
@@ -4,22 +4,42 @@ import platform
 
 from mt import path, shutil
 
-from setuptools import setup, find_packages
+from setuptools import Extension, setup, find_packages
+from Cython.Build import cythonize
 from synexens.version import version
 
+if False:
 if platform.machine() == "x86_64":
     plat = "amd64"
 elif platform.machine() == "aarch64":
     plat = "arm64"
 else:
     raise NotImplementedError
-setup_dirpath = path.dirname(__file__)
-src_dirpath = path.join(setup_dirpath, "sysdk", "lib", plat)
-dst_dirpath = path.join(setup_dirpath, "synexens")
-for filepath in path.glob(src_dirpath + "/*.so"):
-    dst_filepath = path.join(dst_dirpath, path.basename(filepath))
-    path.remove(dst_filepath)
-    shutil.copyfile(filepath, dst_filepath)
+
+if False:
+    setup_dirpath = path.dirname(__file__)
+    src_dirpath = path.join(setup_dirpath, "sysdk", "lib", plat)
+    dst_dirpath = path.join(setup_dirpath, "synexens")
+    filenames = [
+        "libSynexensSDK.so.4.1",
+        "libcsreconstruction2.0.so.2.3",
+        "libSonixCamera.so",
+    ]
+    for filename in filenames:
+        src_filepath = path.join(src_dirpath, filename)
+        dst_filepath = path.join(dst_dirpath, filename)
+        path.remove(dst_filepath)
+        shutil.copyfile(src_filepath, dst_filepath)
+
+extensions = [
+    Extension(
+        "synexens.sysdk",
+        ["synexens/sysdk.pyx"],
+        include_dirs=["sysdk/include"],
+        libraries=["SynexensSDK", "csreconstruction2.0", "SonixCamera"],
+        library_dirs=[path.join("sysdk", "lib", plat)],
+    )
+]
 
 setup(
     name="synexens",
@@ -28,9 +48,10 @@ setup(
     author=["Minh-Tri Pham"],
     packages=find_packages(),
     include_package_data=True,
-    package_data={"": ["*.so"]},
+    package_data={"": ["*.so*"]},
     install_requires=[
         "mtbase",  # for numpy access
         "cython",  # for wrapping purposes
     ],
+    ext_modules=cythonize(extensions, language_level=3),
 )
--- synexens/sysdk.pyx
+++ synexens/sysdk.pyx
@@ -1,7 +1,87 @@
+# distutils: language = c++
+# distutils: language_level = 3
+
 from libcpp cimport bool
 from libcpp.string cimport string
 
-from .sysdk cimport InitSDK, UnInitSDK
+cdef extern from "SYSDKInterface.h" namespace "Synexens" nogil:
+    # ----- constants -----
+
+    # ----- enums -----
+
+    enum SYErrorCode:  # 错误码
+        # 成功
+        SYERRORCODE_SUCCESS = 0,
+        # 失败
+        SYERRORCODE_FAILED = 1,
+        # 设备不存在
+        SYERRORCODE_DEVICENOTEXIST = 2,
+        # 设备未打开
+        SYERRORCODE_DEVICENOTOPENED = 3,
+        # 不支持的分辨率
+        SYERRORCODE_UNKOWNRESOLUTION = 4,
+        # 设备指针句柄为空
+        SYERRORCODE_DEVICEHANDLEEMPTY = 5,
+        # 设备输出数据格式设置失败
+        SYERRORCODE_SETOUTPUTFORMATFAILED = 6,
+        # 获取视频流控制指针失败
+        SYERRORCODE_GETSTREAMCTRLFAILED = 7,
+        # 启动视频流失败
+        SYERRORCODE_STARTSTREAMINGFAILED = 8,
+        # 通讯指针为空
+        SYERRORCODE_COMMUNICATEOBJECTEMPTY = 9,
+        # 无效的SN号
+        SYERRORCODE_UNKOWNSN = 10,
+        # 字符串长度溢出
+        SYERRORCODE_STRINGLENGTHOUTRANGE = 11,
+        # 无效帧类型
+        SYERRORCODE_UNKOWNFRAMETYPE = 12,
+        # 无效设备类型
+        SYERRORCODE_UNKOWNDEVICETYPE = 13,
+        # 设备对象指针为空
+        SYERRORCODE_DEVICEOBJECTEMPTY = 14,
+        # 通知对象指针为空
+        SYERRORCODE_OBSERVEREMPTY = 15,
+        # 通知对象未找到
+        SYERRORCODE_OBSERVERNOTFOUND = 16,
+        # 数量溢出
+        SYERRORCODE_COUNTOUTRANGE = 17,
+        # UVC初始化失败
+        SYERRORCODE_UVCINITFAILED = 18,
+        # UVC查找设备失败
+        SYERRORCODE_UVCFINDDEVICEFAILED = 19,
+        # 无数据帧
+        SYERRORCODE_NOFRAME = 20,
+        # 程序路径获取失败
+        SYERRORCODE_GETAPPFOLDERPATHFAILED = 21,
+        # 视频流未启动
+        SYERRORCODE_NOSTREAMING = 22,
+        # 算法指针为空
+        SYERRORCODE_RECONSTRUCTIONEMPTY = 23,
+        # 视频流已开启
+        SYERRORCODE_STREAMINGEXIST = 24,
+        # 未知的流类型
+        SYERRORCODE_UNKOWNSTREAMTYPE = 25,
+        # 数据指针为空
+        SYERRORCODE_DATABUFFEREMPTY = 26,
+
+
+    # ----- functions -----
+
+
+    # 获取SDK版本号
+    # @ param [in/out] nLength 字符长度
+    # @ param [in/out] pstrSDKVersion SDK版本号字符串指针
+    # @ return 错误码
+    SYErrorCode GetSDKVersion(int& nLength, char* pstrSDKVersion)
+
+    # 初始化SDK
+    # @ return 错误码
+    SYErrorCode InitSDK()
+
+    # 反初始化SDK，释放资源
+    # @ return 错误码
+    SYErrorCode UnInitSDK()
 
 def init_sdk():
     return InitSDK()
